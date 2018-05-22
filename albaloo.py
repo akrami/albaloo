09bbe42abfc400a9ba64b3452c5d2d803b912208
Alireza Akrami <akrami.ui@gmail.com>
2018-05-22 19:15:18 +0430

    ssllab returns error

--- albaloo.py
+++ albaloo.py
@@ -14,7 +14,7 @@ def initialize(website):
     }
     response = requests.get('https://api.ssllabs.com/api/v3/analyze', params=payload)
     if response.status_code == 200:
-        print("initialize successful: "+website+"\n")
+        print("initialize successful: "+website)
         return analyze(website)
     else:
         return 'Not Available'
@@ -30,10 +30,13 @@ def analyze(website):
     if response.status_code == 200:
         json_response = response.json()
         if json_response['status'] == 'READY':
-            print("analyze successful: "+website+"\n")
-            return json.dumps(json_response)
+            print("analyze successful: "+website)
+            return json.dumps(json_response, sort_keys=False, indent=4)
+        elif json_response['status'] == 'ERROR':
+            print("analyze failed: "+website)
+            return json_response['statusMessage']
         else:
-            print("analyze not complete, retry in 20 seconds...\n")
+            print("analyze "+website+", status: "+json_response['status']+", retry in 20 seconds...")
             time.sleep(20)
             return analyze(website)
     else:
@@ -45,12 +48,12 @@ directory = 'results/'
 directory += now.strftime('%Y-%m-%d')
 
 if not os.path.exists(directory):
-    print("make new folder: "+directory+"\n")
+    print("make new folder: "+directory)
     os.makedirs(directory)
 file = open('test.txt', 'r')
 for line in file:
     line = line.strip()
-    print("start checking: "+line+"\n")
+    print("start checking: "+line)
     result = initialize(line)
-    with open(line+'.json', 'w') as f:
+    with open(directory+"/"+line+'.json', 'w') as f:
         f.write(result)
