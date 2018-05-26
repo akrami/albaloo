be65d3bb4170f016ec7179bf1d672b35c040793d
Alireza Akrami <akrami.ui@gmail.com>
2018-05-26 13:44:47 +0430

    OOP
    now every website is an object

--- albaloo.py
+++ albaloo.py
@@ -1,59 +1,19 @@
-import json
-import requests
 import os
 import datetime
-import time
-
-
-def initialize(website):
-    payload = {
-        'host': website,
-        'startNew': 'on',
-        'all': 'done',
-        'maxAge': 23
-    }
-    response = requests.get('https://api.ssllabs.com/api/v3/analyze', params=payload)
-    if response.status_code == 200:
-        print("initialize successful: "+website)
-        return analyze(website)
-    else:
-        return 'Not Available'
-
-
-def analyze(website):
-    payload = {
-        'host': website,
-        'all': 'done',
-        'maxAge': 23
-    }
-    response = requests.get('https://api.ssllabs.com/api/v3/analyze', params=payload)
-    if response.status_code == 200:
-        json_response = response.json()
-        if json_response['status'] == 'READY':
-            print("analyze successful: "+website)
-            return json.dumps(json_response, sort_keys=False, indent=4)
-        elif json_response['status'] == 'ERROR':
-            print("analyze failed: "+website)
-            return json_response['statusMessage']
-        else:
-            print("analyze "+website+", status: "+json_response['status']+", retry in 20 seconds...")
-            time.sleep(20)
-            return analyze(website)
-    else:
-        return 'Not Available'
-
+import website
 
 now = datetime.datetime.now()
 directory = 'results/'
 directory += now.strftime('%Y-%m-%d')
 
 if not os.path.exists(directory):
-    print("make new folder: "+directory)
+    print("make new folder: " + directory)
     os.makedirs(directory)
 file = open('test.txt', 'r')
 for line in file:
     line = line.strip()
-    print("start checking: "+line)
-    result = initialize(line)
-    with open(directory+"/"+line+'.json', 'w') as f:
+    print("start checking: " + line)
+    temp = website.Website(line)
+    result = temp.check_ssllab()
+    with open(directory + "/" + line + '.json', 'w') as f:
         f.write(result)
--- website.py
+++ website.py
@@ -1,3 +1,9 @@
+import json
+import time
+
+import requests
+
+
 class Website:
     """
     Website class
@@ -12,12 +18,40 @@ class Website:
         self.hsts = False
 
     def check_ssllab(self):
-        self.ssllab_result = 'ssllab result'
-        self.ssllab_rating = 'ssllab rating'
-        return True
+        payload = {
+            'host': self.address,
+            'startNew': 'on',
+            'all': 'done',
+            'maxAge': 23
+        }
+        response = requests.get('https://api.ssllabs.com/api/v3/analyze', params=payload)
+        if response.status_code == 200:
+            print("initialize successful: " + self.address)
+            return self.__analyze_ssllab()
+        else:
+            return 'Not Available'
 
-    def analyze_ssllab(self):
-        return True
+    def __analyze_ssllab(self):
+        payload = {
+            'host': self.address,
+            'all': 'done',
+            'maxAge': 23
+        }
+        response = requests.get('https://api.ssllabs.com/api/v3/analyze', params=payload)
+        if response.status_code == 200:
+            json_response = response.json()
+            if json_response['status'] == 'READY':
+                print("analyze successful: " + self.address)
+                return json.dumps(json_response, sort_keys=False, indent=4)
+            elif json_response['status'] == 'ERROR':
+                print("analyze failed: " + self.address)
+                return json_response['statusMessage']
+            else:
+                print("analyze " + self.address + ", status: " + json_response['status'] + ", retry in 20 seconds...")
+                time.sleep(20)
+                return self.__analyze_ssllab()
+        else:
+            return 'Not Available'
 
     def get_ip(self):
         self.ip = '0.0.0.0'
